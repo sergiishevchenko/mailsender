52b6611e9c8477154f236e325ea296a9526d6da3
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2020-03-24 17:32:13 +0100

    fixed html

--- try.py
+++ try.py
@@ -12,15 +12,27 @@ msg['From'] = my_mail
 
 email_list = [line.strip() for line in open('mails.csv')]
 for email in email_list:
-    msg['To'] = email
+    # msg['To'] = email
     html = """\
     <html>
-      <head></head>
       <body>
-        <p>Hi!<br>
-          How are you?<br>
-          Here is the <a href="http://www.python.org">link</a> you wanted.
-        </p>
+        <p>To Whom It May Concern,</p>
+        <p>My name is <strong>Sergii Shevchenko</strong> and I am a <strong>Python Web Developer</strong> with an experience of <strong style="color:red">1 year</strong>.</p>
+        <p>Currently, I am looking for new opportunities as a <strong>Junior Python Developer</strong>.</p>
+        <p>I live in <strong>Lausanne</strong>, canton of <strong>Vaud</strong>.</p>
+        <p>I have permission to live and work in <strong>Switzerland</strong> (<strong style="color:red">Legitimation card R, Permission Ci</strong>).</p>
+        <p>Unfortunately, I don't speak <strong>French</strong> and <strong>German</strong> (only <strong style="color:red">English</strong> and <strong style="color:red">Russian</strong>).</p>
+        <p>My <strong>GitHub</strong> account - <strong>https://github.com/sergiishevchenko</strong></p>
+        <p><strong><i style="color:blue">Should you require further information, please do not hesitate to contack me.</i></strong></p>
+        <p><strong><i style="color:blue">Attached you can find the documents that may interest you.</i></strong></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p>Best regards</p>
+        <p><strong>Sergii Shevchenko</strong></p>
+        <strong>mail:</strong> <strong>sergsheva1704@gmail.com<br></strong>
+        <strong>telegram:</strong> <strong style="color:blue">@sergsheva</strong><br>
+        <strong>tel.:</strong> <strong style="color:blue">+41778154379</strong>
       </body>
     </html>
     """
