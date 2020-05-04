a2414ad241198603e5392b27fc99d66d73872b67
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2020-05-04 10:57:18 +0200

    changed message text

--- try.py
+++ try.py
@@ -10,20 +10,19 @@ msg = MIMEMultipart('alternative')
 msg['Subject'] = "Junior Python Developer"
 msg['From'] = my_mail
 
-email_list = [line.strip() for line in open('mails.csv')]
+email_list = [line.strip() for line in open('mails-1.csv')]
 
 html = """\
 <html>
   <body>
     <p>To Whom It May Concern,</p>
-    <p>My name is Sergii Shevchenko and I am a Python Web Developer with an experience of 1 year.</p>
-    <p>Currently, I am looking for new opportunities as a Junior Python Developer.</p>
-    <p>I live in Lausanne, canton of Vaud.</p>
-    <p>I have permission to live and work in Switzerland (Legitimation card R, Permission Ci).</p>
-    <p>Unfortunately, I don't speak French and German (only English and Russian).</p>
-    <p>My GitHub account - https://github.com/sergiishevchenko</p>
-    <p>Should you require further information, please do not hesitate to contack me.</p>
-    <p>Attached you can find the documents that may interest you.</p>
+    <p>My name is Sergii Shevchenko and I am writing to you to enquire about any possible up and coming openings or opportunities with your company.</p>
+    <p>I have one-year of experience and I am looking for new opportunities as a Junior Python Developer.</p>
+    <p>For your convenience, my enclosed resume contains the details about my qualifications, experience, and skills.</p>
+    <p>In addition, you can find the link to my GitHub account here - https://github.com/sergiishevchenko.</p>
+    <p>I am really interested in working for your company (including voluntary or work experience opportunities).</p>
+    <p>I am fully prepared to commit to any training that might be required.</p>
+    <p>Should you have any questions or require any further information, please do not hesitate to contact me.</p>
     <p></p>
     <p></p>
     <p></p>
@@ -39,9 +38,9 @@ attachment = MIMEText(html, 'html')
 msg.attach(attachment)
 
 # Attach Job permission to the email
-permission = MIMEApplication(open("Job permission.pdf", "rb").read())
-permission.add_header('Content-Disposition', 'attachment', filename="Job permission.pdf")
-msg.attach(permission)
+# permission = MIMEApplication(open("Job permission.pdf", "rb").read())
+# permission.add_header('Content-Disposition', 'attachment', filename="Job permission.pdf")
+# msg.attach(permission)
 
 # Attach Resume to the email
 cv = MIMEApplication(open("sergii-shevchenko.cv.pdf", "rb").read())
@@ -49,9 +48,9 @@ cv.add_header('Content-Disposition', 'attachment', filename="sergii-shevchenko.c
 msg.attach(cv)
 
 # Attach Legitimation card to the email
-card = MIMEApplication(open("sergii-shevchenko.legitimation-card R.pdf", "rb").read())
-card.add_header('Content-Disposition', 'attachment', filename="sergii-shevchenko.legitimation-card R.pdf")
-msg.attach(card)
+# card = MIMEApplication(open("sergii-shevchenko.legitimation-card R.pdf", "rb").read())
+# card.add_header('Content-Disposition', 'attachment', filename="sergii-shevchenko.legitimation-card R.pdf")
+# msg.attach(card)
 
 for email in email_list:
     mail = smtplib.SMTP('smtp.gmail.com', 587)
